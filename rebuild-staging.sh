71baf84195596434944f8a54e3e738d13392c7cc
Yahweh's Delight <121066692+am-muhwezi@users.noreply.github.com>
2026-02-08 06:25:43 +0300

    Delete rebuild-staging.sh

--- rebuild-staging.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f docker-compose.staging.yml pull
-docker compose -f docker-compose.staging.yml down -v
-docker compose -f docker-compose.staging.yml up --build -d
-docker system prune -f
